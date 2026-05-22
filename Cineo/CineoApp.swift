269210110bf127881c92e3f8258a75c7bc3c69a4
Arthur Wunder <95423984+ACWunder@users.noreply.github.com>
2026-05-22 18:59:33 +0200

    Initial Commit

---
+++ Cineo/CineoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineoApp.swift
+//  Cineo
+//
+//  Created by Arthur Wunder on 22.05.26.
+//
+
+import SwiftUI
+
+@main
+struct CineoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
